e3cb322391f46a6496a22a894209cf859131c818
Atul Yadav <atul.yadav@hiddenbrains.in>
2024-09-10 23:52:14 +0530

    Added app launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-          title: Text("My Dashatar App"),
-        ),
-        body: Center(
-          child: Image.asset('images/flutter_dashatar.png'),
+      home: Center(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            title: Text("My Dashatar App"),
+          ),
+          body: Center(
+            child: Image.asset('images/flutter_dashatar.png'),
+          ),
         ),
       ),
     ),
