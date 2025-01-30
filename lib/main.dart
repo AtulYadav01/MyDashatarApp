1577df8d6cc2a64757ee717c351541881a07e45a
Atul Yadav <atul.yadav@hiddenbrains.in>
2025-01-30 13:42:43 +0530

    List and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-          title: Text("My Dashatar App"),
-        ),
-        body: Center(
-          child: Image.asset('images/flutter_dashatar.png'),
-        ),
-      ),
-    ),
-  );
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            title: Text("My Dashatar App"),
+          ),
+          body: Column(
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+                child: Image.asset('images/flutter_dashatar.png'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                child: Image.asset('images/flutter_dashatar.png'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.orange,
+                child: Image.asset('images/flutter_dashatar.png'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.teal,
+                child: Image.asset('images/flutter_dashatar.png'),
+              ),
+            ],
+          )),
+    );
+  }
 }
